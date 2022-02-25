4ea60849a858d9052a296783df3ef3c1ee27ff29
朱伟栋 <weidongzhu36@126.com>
2022-02-25 13:59:00 +0800

    fix: 添加文本
    添加了水电费文本

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link> |水电费
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
